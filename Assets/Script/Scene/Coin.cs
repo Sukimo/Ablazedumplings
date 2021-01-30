3ea6644d56e6bdcfffe2c1ceef9e7d10489c340e
AlphabetX <tanapon.ndf@gmail.com>
2021-01-31 00:23:57 +0700

    add sound in coin

--- Assets/Script/Scene/Coin.cs
+++ Assets/Script/Scene/Coin.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public GameObject _paticleEffect;
+    public AudioClip _sound;
+    private AudioSource _source;
+    private void Start()
+    {
+        _source = Camera.main.GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             if (_paticleEffect)
             {
+                if (_sound) _source.PlayOneShot(_sound);
                 Destroy(Instantiate(_paticleEffect, transform.position, Quaternion.identity).gameObject, 5);
             }
             Destroy(this.gameObject);
