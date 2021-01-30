d6971a1e7897cf30399184e96d9236f69b4701db
AlphabetX <tanapon.ndf@gmail.com>
2021-01-31 00:15:05 +0700

    add coin

---
+++ Assets/Script/Scene/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject _paticleEffect;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (_paticleEffect)
+            {
+                Destroy(Instantiate(_paticleEffect, transform.position, Quaternion.identity).gameObject, 5);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
